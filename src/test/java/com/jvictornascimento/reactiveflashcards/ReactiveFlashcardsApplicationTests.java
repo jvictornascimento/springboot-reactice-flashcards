eb8ad58ae6c1cb4279d1d657fd8c64421313ca50
jvictornascimento <victojoao789@gmail.com>
2025-10-07 02:12:44 -0300

    Start project

---
+++ src/test/java/com/jvictornascimento/reactiveflashcards/ReactiveFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.reactiveflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
