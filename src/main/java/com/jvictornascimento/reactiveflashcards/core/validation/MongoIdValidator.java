686ac073788d28724bfdcf051b479e105f47ea08
jvictornascimento <victojoao789@gmail.com>
2025-10-07 21:01:19 -0300

    feat: Configured validator to MongoId

---
+++ src/main/java/com/jvictornascimento/reactiveflashcards/core/validation/MongoIdValidator.java
@@ -0,0 +1,20 @@
+package com.jvictornascimento.reactiveflashcards.core.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.bson.types.ObjectId;
+@Slf4j
+public class MongoIdValidator implements ConstraintValidator<MongoId, String> {
+    @Override
+    public void initialize(MongoId constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        log.info("==== checking if {} is a velue mongoDB id", value);
+        return StringUtils.isNotBlank(value) && ObjectId.isValid(value);
+    }
+}
