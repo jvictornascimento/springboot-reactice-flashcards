ee2a5675da77fb6d898e4fba9463aedb5647b527
jvictornascimento <victojoao789@gmail.com>
2025-10-08 14:47:55 -0300

    feat: Created default class message and exceptions

---
+++ src/main/java/com/jvictornascimento/reactiveflashcards/domain/exception/BaseErrorMessage.java
@@ -0,0 +1,39 @@
+package com.jvictornascimento.reactiveflashcards.domain.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.ArrayUtils;
+
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+@RequiredArgsConstructor
+public class BaseErrorMessage {
+    private final String DEFAULT_RESOURCE = "message";
+
+    private static final BaseErrorMessage GENERIC_EXCEPTION = new BaseErrorMessage("generic");
+    private static final BaseErrorMessage GENERIC_NOT_FOUND = new BaseErrorMessage("generic.notFound");
+    private static final BaseErrorMessage GENERIC_METHOD_NOT_ALLOW = new BaseErrorMessage("generic.methodNotAllow");
+
+    private final String key;
+
+    private String[] params;
+    public BaseErrorMessage params(final String ... params){
+        this.params = ArrayUtils.clone(params);
+        return this;
+    }
+    public String getMessage(){
+        var message = tryGetMessageFromBundle();
+        if(ArrayUtils.isNotEmpty(params)){
+            final var fmt = new MessageFormat(message);
+            message = fmt.format(params);
+        }
+        return message;
+    }
+    private String tryGetMessageFromBundle(){
+        return getResouce().getString(key);
+    }
+    public ResourceBundle getResouce() {
+        return ResourceBundle.getBundle(DEFAULT_RESOURCE);
+    }
+}
