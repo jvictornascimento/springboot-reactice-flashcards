85910607ab448e3a40dcdc3d89eacffc05cf0e48
jvictornascimento <victojoao789@gmail.com>
2025-10-07 21:00:29 -0300

    feat: Configured converter to Mongo

--- src/main/java/com/jvictornascimento/reactiveflashcards/ReactiveFlashcardsApplication.java
+++ src/main/java/com/jvictornascimento/reactiveflashcards/ReactiveFlashcardsApplication.java
@@ -2,8 +2,10 @@ package com.jvictornascimento.reactiveflashcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
+@EnableReactiveMongoAuditing(dateTimeProviderRef = "dateTimeProvider")
 public class ReactiveFlashcardsApplication {
 
 	public static void main(String[] args) {
